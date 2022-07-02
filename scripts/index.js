7c469e1da35a4852a6fa75ef604481838a272d26
Oxana Shovkoplyas <ekmati@yandex.ru>
2022-07-02 21:03:38 +0300

    after 1 review

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,27 @@
 const popupElement = document.querySelector('.popup');
 const profileElement = document.querySelector('.profile');
-let popupInputName = popupElement.querySelector('.popup__input_name');
-let popupInputRole = popupElement.querySelector('.popup__input_role');
+let popupInputName = popupElement.querySelector('.popup__input_field_name');
+let popupInputRole = popupElement.querySelector('.popup__input_field_role');
 let profileName = profileElement.querySelector('.profile__name');
 let profileRole = profileElement.querySelector('.profile__role');
 const popupClose = popupElement.querySelector('.popup__close');
 const profileEdit = profileElement.querySelector('.profile__edit-profile');
 const formElement = popupElement.querySelector('.popup__form');
 
-profileEdit.addEventListener('click', function () {
+function openPopup() {
     popupElement.classList.add('popup_open');
-});
+};
 
 function closeWindow() {
     popupElement.classList.remove('popup_open');
 };
-
-popupClose.addEventListener('click', closeWindow);
-
-formElement.addEventListener('submit', function(evt) {
+function saveButtonHandler(evt) {
     evt.preventDefault();
     profileName.textContent = popupInputName.value;
     profileRole.textContent = popupInputRole.value;
     closeWindow();
-});
+};
+
+popupClose.addEventListener('click', closeWindow);
+formElement.addEventListener('submit', saveButtonHandler);
+profileEdit.addEventListener('click', openPopup);
